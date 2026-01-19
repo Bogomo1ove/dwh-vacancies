06b134a8d2b939d4257f01133e85ee6e3ea036a1
Vladislav <vladbogenka@yandex.ru>
2026-01-19 16:25:22 +0300

    Update from_raw_to_s3.py

--- from_raw_to_s3.py
+++ from_raw_to_s3.py
@@ -1,14 +1,12 @@
+from parser_hh import HeadHunter
+from parser_geekjob import GeekJob
 from minio_conn import Bucket, Object
 import io
 import json
 
 
 def from_source_to_s3(source, date,  **kwargs):
-    if source.bucket_name in Bucket.list_buckets():
-        data = io.BytesIO(json.dumps(source.get_vacancies(**kwargs), ensure_ascii=False).encode('utf-8'))
-        Object.upload(source.bucket_name, f'{date.replace(microsecond=0).isoformat()}.json', data)
-    else:
+    if source.bucket_name not in Bucket.list_buckets():
         Bucket.create(source.bucket_name)
-        data = io.BytesIO(json.dumps(source.get_vacancies(**kwargs), ensure_ascii=False).encode('utf-8'))
-        Object.upload(source.bucket_name, f'{date.replace(microsecond=0).isoformat()}.json', data)
-        
+    data = io.BytesIO(json.dumps(source.get_vacancies(**kwargs), ensure_ascii=False).encode('utf-8'))
+    Object.upload(source.bucket_name, f'{date.replace(microsecond=0).isoformat()}.json', data)     
