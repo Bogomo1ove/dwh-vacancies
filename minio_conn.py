326ae74c18a98e64d5e9e04fd79a18d8fa9a629f
Vladislav <vladbogenka@yandex.ru>
2026-01-19 16:09:23 +0300

    Update minio_conn.py

--- minio_conn.py
+++ minio_conn.py
@@ -1,45 +1,50 @@
 from minio import Minio
-from minio.error import S3Error
 from config import access_key,secret_key
+from typing import Any
 
 
-minio_client = Minio(
-    endpoint="localhost:9000",
-    access_key=access_key,
-    secret_key=secret_key,
-    secure=False  
-)
+class MinioConnector:
+    minio_client = Minio(
+        endpoint="localhost:9000",
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=False)
+
+class Bucket:
+
+    @staticmethod
+    def create(bucket_name) -> None:
+        MinioConnector.minio_client.make_bucket(bucket_name)
+        print(f"Bucket {bucket_name} created")
+
+    @staticmethod
+    def list_buckets():
+        return [bucket.name for bucket in MinioConnector.minio_client.list_buckets()]
 
 
-def create_bucket(bucket_name) -> None:
-    minio_client.make_bucket(bucket_name)
-    print(f"Bucket {bucket_name} created")
+    @staticmethod
+    def remove(bucket_name: str) -> None:
+        MinioConnector.minio_client.remove_bucket(bucket_name)
+        print(f"Bucket {bucket_name} removed")
 
-def list_buckets():
-    try:
-        buckets = minio_client.list_buckets()
-        for bucket in buckets:
-            print(bucket.name, bucket.creation_date)
-    except S3Error as err:
-        print(err)
 
-def upload_object(bucket_name: str,
-                  file_path: str,
-                  object_name: str) -> None:
-    minio_client.fput_object(bucket_name, object_name, file_path)
-    print(f"File '{file_path}' uploaded")
 
-def download_object(bucket_name: str,
-                  file_path: str,
-                  object_name: str) -> None:
-    minio_client.fget_object(bucket_name, file_path, object_name)
-    print(f"File '{object_name}' downloaded")
+class Object:
 
-def remove_object(bucket_name: str, object_name: str) -> None:
-    minio_client.remove_object(bucket_name, object_name)
-    print(f"Object '{object_name}' deleted")
+    @staticmethod
+    def upload(bucket_name: str,
+               object_name: str,
+               data: Any) -> None:
+        MinioConnector.minio_client.put_object(bucket_name, object_name, data, length=-1, part_size=5*1024*1024)
 
-def remove_bucket(bucket_name: str) -> None:
-    minio_client.remove_bucket(bucket_name)
-    print(f"Bucket {bucket_name} removed")
+    @staticmethod
+    def download(bucket_name: str,
+                        file_path: str,
+                        object_name: str) -> None:
+        MinioConnector.minio_client.fget_object(bucket_name, file_path, object_name)
+        print(f"File '{object_name}' downloaded")
 
+    @staticmethod
+    def remove(bucket_name: str, object_name: str) -> None:
+        MinioConnector.minio_client.remove_object(bucket_name, object_name)
+        print(f"Object '{object_name}' deleted")
